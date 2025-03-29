8949b38a15ffae13929d1b92db85184ef3b106f6
Luiartinez <luismartinez13hernandez@gmail.com>
2025-03-29 17:43:55 -0600

    producto/controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,26 +1,31 @@
 using System.Diagnostics;
+using Managerproduct.Models;
 using Microsoft.AspNetCore.Mvc;
-using ProyectoCatedra.Db;
-using ProyectoCatedra.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
-namespace ProyectoCatedra.Controllers
+namespace Managerproduct.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly AppDbContext appDbContext;
+        private readonly AplicationDbContext _context;
 
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-
-            return View();
+            var productos = await _context.Productos
+                .Include(p => p.Categoria)
+                .Include(p => p.Proveedor)
+                .ToListAsync();
+            return View(productos); // Asegúrate de pasar 'productos' a la vista
         }
+
 
         public IActionResult Privacy()
         {
@@ -32,7 +37,31 @@ namespace ProyectoCatedra.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Create()
+        {
+            ViewBag.Categoriaid = new SelectList(_context.Categorias, "Id", "Nombre");
+            ViewBag.Proveedorid = new SelectList(_context.Proveedores, "Id", "Nombre");
+            return View(new Producto());
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Productos.Add(producto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index"); // Asegúrate de que esto esté presente
+            }
+            // Si hay error, recarga los SelectList
+            ViewBag.Categoriaid = new SelectList(_context.Categorias, "Id", "Nombre", producto.Categoriaid);
+            ViewBag.Proveedorid = new SelectList(_context.Proveedores, "Id", "Nombre", producto.Proveedorid);
+            return View(producto);
+        }
+
+
     }
 }
-
-
