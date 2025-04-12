5e357b8eb86b127e3fc5fd621e52e5cb070626e3
Luiartinez <luismartinez13hernandez@gmail.com>
2025-04-11 21:37:17 -0600

    DB-HistorialMovimientos

--- Db/AppDbContext.cs
+++ Db/AppDbContext.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using ProyectoCatedra.Models;
 
 namespace ProyectoCatedra.Db
@@ -12,11 +12,16 @@ namespace ProyectoCatedra.Db
         public DbSet<Empleados> Empleados { get; set; }
         public DbSet<RecuperacionContraseña> RecuperacionContraseñas { get; set; }
         public DbSet<Producto> Productos { get; set; }
+        public DbSet<HistorialMovimiento> HistorialMovimientos { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Producto>()
                 .Property(p => p.Precio)
                 .HasColumnType("decimal(18,2)"); // Definir precisión y escala
+
+            modelBuilder.Entity<HistorialMovimiento>()
+           .Property(h => h.FechaMovimiento)
+           .HasDefaultValueSql("GETDATE()");
 
             base.OnModelCreating(modelBuilder);
         }
