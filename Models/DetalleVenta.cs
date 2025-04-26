62b525ea14eef05d89da4f0dd06a1725a5111bbc
Luiartinez <luismartinez13hernandez@gmail.com>
2025-04-25 21:45:44 -0600

    Detalle-venta

---
+++ Models/DetalleVenta.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoCatedra.Models
+{
+    public class DetalleVenta
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int VentaId { get; set; }
+
+        [ForeignKey("VentaId")]
+        public Venta Venta { get; set; }
+
+        [Required]
+        public int ProductoId { get; set; }
+
+        [ForeignKey("ProductoId")]
+        public Producto Producto { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int Cantidad { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal PrecioUnitario { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Subtotal { get; set; } // Cambiado a propiedad de lectura/escritura
+    }
+}
