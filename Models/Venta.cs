8c64d8c2c992b6f33b2ee383338ced9011ed9fa6
Luiartinez <luismartinez13hernandez@gmail.com>
2025-04-25 21:47:07 -0600

    DB-venta

---
+++ Models/Venta.cs
@@ -0,0 +1,17 @@
+﻿namespace ProyectoCatedra.Models
+{
+    public class Venta
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public decimal Total { get; set; }
+
+        // Relación con cliente (puede ser nula para ventas sin registro)
+        public int? ClienteId { get; set; }
+        public Cliente Cliente { get; set; }
+
+        // Detalles de la venta
+        public ICollection<DetalleVenta> Detalles { get; set; }
+        public DateTime FechaVenta { get; internal set; }
+    }
+}
