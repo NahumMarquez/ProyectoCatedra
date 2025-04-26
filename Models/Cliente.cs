16e64b9ce7816544db9cae19223739279853c4b4
Luiartinez <luismartinez13hernandez@gmail.com>
2025-04-25 21:44:52 -0600

    Modelo-cliente-datos

---
+++ Models/Cliente.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoCatedra.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        public string Apellido { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        public string Telefono { get; set; }
+
+        public string Direccion { get; set; }
+
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
+
+        // Propiedad de navegación para las ventas del cliente
+        public ICollection<Venta> Ventas { get; set; } = new List<Venta>();
+    }
+}
