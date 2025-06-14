3631e41b5b2f03257bd99a649d4e14f8648f1f8c
Luis Alexander Hernández Martinez <luismartinez13hernandez@gmail.com>
2025-06-14 17:37:19 -0600

    Revert "EstilosAgregados"
    This reverts commit aff34638d6042fe6f524d9e7f4a7654d90e8687b.

---
+++ Migrations/20250328030950_CorregirColumnas.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoCatedra.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorregirColumnas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Token",
+                table: "RecuperacionContraseñas",
+                newName: "token");
+
+            migrationBuilder.RenameColumn(
+                name: "Expiracion",
+                table: "RecuperacionContraseñas",
+                newName: "fecha_expiracion");
+
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "RecuperacionContraseñas",
+                newName: "correo");
+
+            migrationBuilder.RenameColumn(
+                name: "Usuario",
+                table: "Empleados",
+                newName: "usuario");
+
+            migrationBuilder.RenameColumn(
+                name: "Rol",
+                table: "Empleados",
+                newName: "rol");
+
+            migrationBuilder.RenameColumn(
+                name: "Correo",
+                table: "Empleados",
+                newName: "correo");
+
+            migrationBuilder.RenameColumn(
+                name: "Contraseña",
+                table: "Empleados",
+                newName: "contraseña");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "token",
+                table: "RecuperacionContraseñas",
+                newName: "Token");
+
+            migrationBuilder.RenameColumn(
+                name: "fecha_expiracion",
+                table: "RecuperacionContraseñas",
+                newName: "Expiracion");
+
+            migrationBuilder.RenameColumn(
+                name: "correo",
+                table: "RecuperacionContraseñas",
+                newName: "Email");
+
+            migrationBuilder.RenameColumn(
+                name: "usuario",
+                table: "Empleados",
+                newName: "Usuario");
+
+            migrationBuilder.RenameColumn(
+                name: "rol",
+                table: "Empleados",
+                newName: "Rol");
+
+            migrationBuilder.RenameColumn(
+                name: "correo",
+                table: "Empleados",
+                newName: "Correo");
+
+            migrationBuilder.RenameColumn(
+                name: "contraseña",
+                table: "Empleados",
+                newName: "Contraseña");
+        }
+    }
+}
